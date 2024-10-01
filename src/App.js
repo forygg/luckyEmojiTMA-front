538e4303e6ad14166c94941ae351861c7e93a4fb
Evgeniy Spiridonov <e.spiridonov@innopolis.university>
2024-10-01 20:52:43 +0300

    telegram mini app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const tg = window.Telegram.WebApp;
+    tg.ready();
+    tg.expand(); // Expands the app to full-screen
+  }, []);
+
+  const closeApp = () => {
+    window.Telegram.WebApp.close();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-2xl font-bold mb-4">Telegram MiniApp</h1>
+        <button
+            className="px-4 py-2 bg-blue-500 text-white rounded-md"
+            onClick={closeApp}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Close MiniApp
+        </button>
+      </div>
   );
 }
 
