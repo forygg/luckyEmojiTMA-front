04b1131997fa3ea3716001ff9f25fae01a79e344
srmrkN <maks.yarcheev@mail.ru>
2024-10-04 00:27:15 +0300

    GameCards fix, the page is 99% ready

--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -3,9 +3,10 @@ import "./gameCard.css";
 const GameCard = ({ title, description, icon }) => {
     return (
         <div className="game-card">
-            <img src={icon} alt={`${title} icon`} />
-            <h3>{title}</h3>
+            <img src={icon} alt={`${title} icon`}/>
             <p>{description}</p>
+            <div className="game-card-divider"></div>
+            <h3>{title}</h3>
         </div>
     );
 };
--- src/components/GameCards/GameCards.js
+++ src/components/GameCards/GameCards.js
@@ -1,19 +1,20 @@
 import GameCard from '../GameCard/GameCard';
 import ticket from '../../assets/ticket.svg';
-import "./gameCards.css";  // Import the custom CSS file
+import Emoji from '../../assets/gc_Emoji.png';
+import "./gameCards.css";
 
 const GameCards = () => {
     return (
         <div className="game-cards-container">
             <GameCard
-                title="LuckyTicket"
-                description="Win every hour! Guaranteed prize for all participants."
                 icon={ticket}
+                description="Win every hour! Guaranteed prize for all participants."
+                title="LuckyTicket"
             />
             <GameCard
                 title="LuckyEmoji"
-                description="How fast are you? Catch several emoji in a row..."
-                icon={ticket}
+                description="How fast are you? Catch several Emoji in a row, at least three."
+                icon={Emoji}
             />
         </div>
     );
