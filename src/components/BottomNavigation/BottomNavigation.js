381f8b03699d45a59bb514e3fb304fe31003bf89
Evgeniy Spiridonov <e.spiridonov@innopolis.university>
2024-10-08 00:45:11 +0300

    Added leader (fix)

--- src/components/BottomNavigation/BottomNavigation.js
+++ src/components/BottomNavigation/BottomNavigation.js
@@ -25,26 +25,41 @@ const BottomNavigation = ({ onTabChange, hasNewTasks }) => {
 
     return (
         <div className="bottom-navigation">
-            <div className={"nav-item"} onClick={() => handleTabClick('Profile')}>
+            <div
+                className={`nav-item ${activeTab === 'Profile' ? 'active' : ''}`}
+                onClick={() => handleTabClick('Profile')}
+            >
                 <img src={getIcon('Profile', profileIcon, profileIconActive)} alt="Profile" className="nav-icon" />
-                <p className={activeTab === 'Profile' ? 'nav-text-selected' : 'nav-text'}>Profile</p>
+                <p className={`nav-text ${activeTab === 'Profile' ? 'active' : ''}`}>Profile</p>
             </div>
-            <div className="nav-item" onClick={() => handleTabClick('Tasks')}>
+            <div
+                className={`nav-item ${activeTab === 'Tasks' ? 'active' : ''}`}
+                onClick={() => handleTabClick('Tasks')}
+            >
                 <img src={getIcon('Tasks', tasksIcon, tasksIconActive)} alt="Tasks" className="nav-icon" />
                 {hasNewTasks && <div className="red-dot"></div>}
-                <p className={activeTab === 'Tasks' ? 'nav-text-selected' : 'nav-text'}>Tasks</p>
+                <p className={`nav-text ${activeTab === 'Tasks' ? 'active' : ''}`}>Tasks</p>
             </div>
-            <div className="nav-item" onClick={() => handleTabClick('Games')}>
+            <div
+                className={`nav-item ${activeTab === 'Games' ? 'active' : ''}`}
+                onClick={() => handleTabClick('Games')}
+            >
                 <img src={getIcon('Games', gamesIcon, gamesIconActive)} alt="Games" className="nav-icon" />
-                <p className={activeTab === 'Games' ? 'nav-text-selected' : 'nav-text'}>Games</p>
+                <p className={`nav-text ${activeTab === 'Games' ? 'active' : ''}`}>Games</p>
             </div>
-            <div className="nav-item" onClick={() => handleTabClick('Leader')}>
+            <div
+                className={`nav-item ${activeTab === 'Leader' ? 'active' : ''}`}
+                onClick={() => handleTabClick('Leader')}
+            >
                 <img src={getIcon('Leader', leaderIcon, leaderIconActive)} alt="Leader" className="nav-icon" />
-                <p className={activeTab === 'Leader' ? 'nav-text-selected' : 'nav-text'}>Leader</p>
+                <p className={`nav-text ${activeTab === 'Leader' ? 'active' : ''}`}>Leader</p>
             </div>
-            <div className="nav-item" onClick={() => handleTabClick('PayTable')}>
+            <div
+                className={`nav-item ${activeTab === 'PayTable' ? 'active' : ''}`}
+                onClick={() => handleTabClick('PayTable')}
+            >
                 <img src={getIcon('PayTable', paytableIcon, paytableIconActive)} alt="Pay Table" className="nav-icon" />
-                <p className={activeTab === 'PayTable' ? 'nav-text-selected' : 'nav-text'}>Pay Table</p>
+                <p className={`nav-text ${activeTab === 'PayTable' ? 'active' : ''}`}>Pay Table</p>
             </div>
         </div>
     );
--- src/components/Leader/Leader.js
+++ src/components/Leader/Leader.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './leader.css'; // Make sure this is where the CSS is stored
 
 const Leader = () => {
-    return <div>Leader Content</div>;
-};
+    const mockAvatar = "https://www.nchti.ru/wp-content/images/profile-anonymous2.png";
 
-export default Leader;
+    const mockData = [
+        { id: 1, name: 'Full Name From Telegram', score: 100000, icon: mockAvatar },
+        { id: 2, name: 'Sergei Familievichkin', score: 75203, icon: mockAvatar },
+        { id: 3, name: 'Olga Martinunks', score: 1200, icon: mockAvatar },
+        { id: 4, name: 'Ivan Ivanovich', score: 400, icon: mockAvatar },
+        { id: 5, name: 'Full Name From Telegram', score: 395, icon: mockAvatar },
+        { id: 6, name: 'Sergei Familievichkin', score: 310, icon: mockAvatar },
+        { id: 7, name: 'Olga Martinunks', score: 220, icon: mockAvatar },
+        { id: 8, name: 'Ivan Ivanovich', score: 50, icon: mockAvatar },
+    ];
+
+    const [leaders, setLeaders] = useState(mockData);
+
+    // useEffect(() => {
+    //     fetchLeaders();
+    // });
+    //
+    // const fetchLeaders = () => {
+    //     //TODO: Fetch leaders data
+    //     setLeaders(mockData);
+    // };
+
+    return (
+        <div className="leaderboard-container">
+            <div className="leaderboard-text">
+                You can be the first!
+            </div>
+            <div className="leaderboard-text">
+                The TOP-10 players in the game receive daily
+                bonuses.
+            </div>
+                <div className="leaderboard">
+                    {leaders.map((leader) => (
+                        <div key={leader.id} className="leader-item">
+                            <div className="leader-avatar">
+                                <img src={leader.icon} alt={leader.name}/>
+                            </div>
+                            <div className="leader-info">
+                                <div className="leader-name">{leader.name}</div>
+                            </div>
+                            <div className="leader-balance">{leader.score.toLocaleString()} +LEP</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            );
+            };
+
+            export default Leader;
